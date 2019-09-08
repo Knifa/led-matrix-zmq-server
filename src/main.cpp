1e27fef294b98a10f1b4a85ca4fe164ffae18ec2
Knifa <knifacat@gmail.com>
2019-09-08 21:58:46 +0100

    Pull all matrix settings from ENV

--- src/main.cpp
+++ src/main.cpp
@@ -8,20 +8,53 @@
 
 volatile bool running = true;
 
-void handle_interrupt(int signo)
+void handle_interrupt(const int signo)
 {
     running = false;
 }
 
-int env_arg_atoi_or_zero(char *name)
+int env_arg_atoi_or_default(const char *name, const int d = 0)
 {
-    char *arg_str = getenv(name);
+    char *arg_str = std::getenv(name);
     if (arg_str == NULL)
     {
-        return 0;
+        return d;
     }
 
-    return atoi(arg_str);
+    return std::atoi(arg_str);
+}
+
+const char *env_arg_or_default(const char *name, const char *d = NULL)
+{
+    char *arg_str = std::getenv(name);
+    if (arg_str == NULL)
+    {
+        return d;
+    }
+
+    return arg_str;
+}
+
+void set_options_from_env(rgb_matrix::RGBMatrix::Options *matrix_opts, rgb_matrix::RuntimeOptions *runtime_opts)
+{
+    matrix_opts->brightness = env_arg_atoi_or_default("MATRIX_BRIGHTNESS", matrix_opts->brightness);
+    matrix_opts->chain_length = env_arg_atoi_or_default("MATRIX_CHAIN_LENGTH", matrix_opts->chain_length);
+    matrix_opts->cols = env_arg_atoi_or_default("MATRIX_COLS", matrix_opts->cols);
+    matrix_opts->disable_hardware_pulsing = env_arg_atoi_or_default("MATRIX_DISABLE_HARDWARE_PULSING", matrix_opts->disable_hardware_pulsing);
+    matrix_opts->hardware_mapping = env_arg_or_default("MATRIX_HARDWARE_MAPPING", matrix_opts->hardware_mapping);
+    matrix_opts->led_rgb_sequence = env_arg_or_default("MATRIX_LED_SEQUENCE", matrix_opts->led_rgb_sequence);
+    matrix_opts->multiplexing = env_arg_atoi_or_default("MATRIX_MULTIPLEXING", matrix_opts->multiplexing);
+    matrix_opts->panel_type = env_arg_or_default("MATRIX_PANEL_TYPE", matrix_opts->panel_type);
+    matrix_opts->parallel = env_arg_atoi_or_default("MATRIX_PARALLEL", matrix_opts->parallel);
+    matrix_opts->pixel_mapper_config = env_arg_or_default("MATRIX_PIXEL_MAPPER_CONFIG", matrix_opts->pixel_mapper_config);
+    matrix_opts->pwm_bits = env_arg_atoi_or_default("MATRIX_PWM_BITS", matrix_opts->pwm_bits);
+    matrix_opts->pwm_dither_bits = env_arg_atoi_or_default("MATRIX_PWM_DITHER_BITS", matrix_opts->pwm_dither_bits);
+    matrix_opts->pwm_lsb_nanoseconds = env_arg_atoi_or_default("MATRIX_PWM_LSB_NANOSECONDS", matrix_opts->pwm_lsb_nanoseconds);
+    matrix_opts->row_address_type = env_arg_atoi_or_default("MATRIX_ROW_ADDRESS_TYPE", matrix_opts->row_address_type);
+    matrix_opts->rows = env_arg_atoi_or_default("MATRIX_ROWS", matrix_opts->rows);
+    matrix_opts->scan_mode = env_arg_atoi_or_default("MATRIX_SCAN_MODE", matrix_opts->scan_mode);
+
+    runtime_opts->gpio_slowdown = env_arg_atoi_or_default("MATRIX_GPIO_SLOWDOWN", runtime_opts->gpio_slowdown);
 }
 
 int main(int argc, char *argv[])
@@ -29,22 +62,23 @@ int main(int argc, char *argv[])
     std::signal(SIGINT, handle_interrupt);
     std::signal(SIGTERM, handle_interrupt);
 
-    rgb_matrix::RGBMatrix::Options led_options;
-    rgb_matrix::RuntimeOptions runtime;
+    rgb_matrix::RGBMatrix::Options matrix_opts;
+    rgb_matrix::RuntimeOptions runtime_opts;
+    set_options_from_env(&matrix_opts, &runtime_opts);
 
-    if (!rgb_matrix::ParseOptionsFromFlags(&argc, &argv, &led_options, &runtime))
+    if (!rgb_matrix::ParseOptionsFromFlags(&argc, &argv, &matrix_opts, &runtime_opts))
     {
         rgb_matrix::PrintMatrixFlags(stdout);
         return 1;
     }
 
-    rgb_matrix::RGBMatrix *matrix = CreateMatrixFromOptions(led_options, runtime);
+    auto matrix = CreateMatrixFromOptions(matrix_opts, runtime_opts);
     if (matrix == NULL)
     {
         return 1;
     }
 
-    rgb_matrix::FrameCanvas *canvas = matrix->CreateFrameCanvas();
+    auto canvas = matrix->CreateFrameCanvas();
 
     zsock_t *zrep = zsock_new_rep("tcp://*:8182");
 
@@ -56,13 +90,13 @@ int main(int argc, char *argv[])
         zsock_recv(zrep, "b", &image, &image_size);
         zsock_send(zrep, "z");
 
-        for (int y = 0; y < 32; y++)
+        for (int y = 0; y < canvas->height(); y++)
         {
-            for (int x = 0; x < 64; x++)
+            for (int x = 0; x < canvas->width(); x++)
             {
-                int r = image[y * 64 * 4 + x * 4 + 2];
-                int g = image[y * 64 * 4 + x * 4 + 1];
-                int b = image[y * 64 * 4 + x * 4 + 0];
+                int r = image[y * canvas->height() * 4 + x * 4 + 2];
+                int g = image[y * canvas->height() * 4 + x * 4 + 1];
+                int b = image[y * canvas->height() * 4 + x * 4 + 0];
 
                 r = (int)(pow(r / 255.0f, 2.2f) * 255.0f);
                 g = (int)(pow(g / 255.0f, 2.2f) * 255.0f);
@@ -78,8 +112,7 @@ int main(int argc, char *argv[])
         }
 
         canvas = matrix->SwapOnVSync(canvas);
-
-        free(image);
+        delete image;
     }
 
     delete matrix;
