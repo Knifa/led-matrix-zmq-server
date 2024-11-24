8affb0578ab0f7cfc77d32e5213b2d1e1b6c3968
Knifa <knifa@users.noreply.github.com>
2024-11-24 18:23:00 +0000

    base bpp off uint32_t size

--- src/consts.hpp
+++ src/consts.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
+#include <cstdint>
+
 namespace consts {
-constexpr auto BPP = 4;
+constexpr auto BPP = sizeof(std::uint32_t);
 
 const auto DEFAULT_CONTROL_ENDPOINT = "ipc:///tmp/matrix-control.sock";
 const auto DEFAULT_FRAME_ENDPOINT = "ipc:///tmp/matrix-frames.sock";
