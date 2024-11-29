d454018ce870c57ee9e402be6e1cc40c67ef23f6
Knifa <knifa@users.noreply.github.com>
2024-11-29 12:37:40 +0000

    change default socket location to me more legit

--- src/consts.hpp
+++ src/consts.hpp
@@ -3,9 +3,11 @@
 #include <cstdint>
 
 namespace consts {
+
 constexpr auto pixel_size = sizeof(std::uint32_t);
 constexpr auto bpp = 8 * pixel_size;
 
-const auto default_control_endpoint = "ipc:///tmp/matrix-control.sock";
-const auto default_frame_endpoint = "ipc:///tmp/matrix-frames.sock";
+const auto default_control_endpoint = "ipc:///run/led-matrix-zmq-control.sock";
+const auto default_frame_endpoint = "ipc:///run/led-matrix-zmq-frame.sock";
+
 } // namespace consts
